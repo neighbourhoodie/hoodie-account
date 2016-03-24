2d40a25a5584872c118d6205eb900dacb07959a2
finnp <derfinn@gmail.com>
2016-03-25 00:26:10 +0100

    fix(example): Makes the example server work correctly
    The server.register was running after server.start and therefore not working.
    Thanks to @gregor for figuring that out (https://github.com/hoodiehq/hoodie/issues/458#issuecomment-201057864)
    
    Also the PouchDB constructor shouldn't get an URL, but simply a name.

--- example/index.js
+++ example/index.js
@@ -25,49 +25,56 @@ browserify.bundle(bundleEE.emit.bind(bundleEE, 'done'))
 
 var server = new Hapi.Server({})
 
-var db = new PouchDB('http://localhost:5984/_users', {db: memdown})
+var db = new PouchDB('_users', {db: memdown})
 
 db.installUsersBehavior().then(function () {
+  server.connection({
+    port: 4663
+  })
+
   server.register({
     register: hapiAccount,
     options: {
-      usersDB: db
+      usersDb: db
+    },
+    routes: {
+      prefix: '/hoodie/account/api'
     }
   }, function (err) {
     if (err) throw err
   })
-})
 
-server.connection({
-  port: 4663
-})
-
-server.route({
-  method: 'GET',
-  path: '/hoodie/client.js',
-  handler: function (request, reply) {
-    if (!ACCOUNTJS) {
-      return bundleEE.once('done', function (error, buffer) {
-        if (!error) reply(buffer).type('application/javascript')
-      })
+  server.route({
+    method: 'GET',
+    path: '/hoodie/client.js',
+    handler: function (request, reply) {
+      if (!ACCOUNTJS) {
+        return bundleEE.once('done', function (error, buffer) {
+          if (!error) reply(buffer).type('application/javascript')
+        })
+      }
+      reply(ACCOUNTJS).type('application/javascript')
     }
-    reply(ACCOUNTJS).type('application/javascript')
-  }
+  })
+
+  server.register(inert, function (err) {
+    if (err) throw err
+  })
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+      reply.file(path.join(__dirname, '..', 'public', 'index.html'))
+    }
+  })
+
+  server.start(function (err) {
+    if (err) throw err
+    console.log('Server running at %s', server.info.uri)
+  })
 })
 
-server.register(inert, function (err) {
-  if (err) throw err
-})
-
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    reply.file(path.join(__dirname, '..', 'public', 'index.html'))
-  }
-})
-
-server.start(function (err) {
-  if (err) throw err
-  console.log('Server running at %s', server.info.uri)
+.catch(function (error) {
+  throw error
 })
