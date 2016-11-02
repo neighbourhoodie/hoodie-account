0ac2d4beebe19adbcb2822f5f996fddb6326651d
Gregor <gregor@martynus.net>
2016-11-02 15:43:06 -0700

    fix: adapt for latest @hoodie/account-server

--- example/index.js
+++ example/index.js
@@ -1,12 +1,11 @@
+var EventEmitter = require('events').EventEmitter
 var Hapi = require('hapi')
-var PouchDB = require('pouchdb')
-var memdown = require('memdown')
+var hapiAccount = require('../server')
 var inert = require('inert')
 var path = require('path')
-var EventEmitter = require('events').EventEmitter
-var hapiAccount = require('../server')
-
-PouchDB.plugin(require('pouchdb-users'))
+var PouchDB = require('pouchdb-core')
+  .plugin(require('pouchdb-mapreduce'))
+  .plugin(require('pouchdb-adapter-memory'))
 
 var browserify = require('browserify')([], {
   standalone: 'hoodie'
@@ -25,57 +24,50 @@ browserify.bundle(bundleEE.emit.bind(bundleEE, 'done'))
 
 var server = new Hapi.Server({})
 
-var db = new PouchDB('_users', {db: memdown})
-
-db.installUsersBehavior().then(function () {
-  server.connection({
-    port: 4663
-  })
-
-  server.register({
-    register: hapiAccount,
-    options: {
-      usersDb: db,
-      secret: 'NOT SO SECRET'
-    },
-    routes: {
-      prefix: '/hoodie/account/api'
-    }
-  }, function (err) {
-    if (err) throw err
-  })
-
-  server.route({
-    method: 'GET',
-    path: '/hoodie/client.js',
-    handler: function (request, reply) {
-      if (!ACCOUNTJS) {
-        return bundleEE.once('done', function (error, buffer) {
-          if (!error) reply(buffer).type('application/javascript')
-        })
-      }
-      reply(ACCOUNTJS).type('application/javascript')
-    }
-  })
-
-  server.register(inert, function (err) {
-    if (err) throw err
-  })
-
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-      reply.file(path.join(__dirname, '..', 'public', 'index.html'))
-    }
-  })
-
-  server.start(function (err) {
-    if (err) throw err
-    console.log('Server running at %s', server.info.uri)
-  })
+server.connection({
+  port: 4663
 })
 
-.catch(function (error) {
-  throw error
+server.register({
+  register: hapiAccount,
+  options: {
+    PouchDB: PouchDB,
+    usersDb: '_users',
+    secret: 'NOT SO SECRET'
+  },
+  routes: {
+    prefix: '/hoodie/account/api'
+  }
+}, function (err) {
+  if (err) throw err
+})
+
+server.route({
+  method: 'GET',
+  path: '/hoodie/client.js',
+  handler: function (request, reply) {
+    if (!ACCOUNTJS) {
+      return bundleEE.once('done', function (error, buffer) {
+        if (!error) reply(buffer).type('application/javascript')
+      })
+    }
+    reply(ACCOUNTJS).type('application/javascript')
+  }
+})
+
+server.register(inert, function (err) {
+  if (err) throw err
+})
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: function (request, reply) {
+    reply.file(path.join(__dirname, '..', 'public', 'index.html'))
+  }
+})
+
+server.start(function (err) {
+  if (err) throw err
+  console.log('Server running at %s', server.info.uri)
 })
